875e1dfe4c94d9dfb9f49f5a02d0e28e87b249ce
aleksei <leshadjet@mail.ru>
2022-08-17 21:25:25 +0700

    0.1

---
+++ app.js
@@ -0,0 +1,12 @@
+ $(document).ready(function(){
+
+  // function activ_btn_search(){
+  //     if ( $(".search_button input").hasClass("activ") ) {
+  //       $('.search_button input').removeClass('activ');
+  //   } else{
+  //       $('.search_button input').addClass('activ');
+  //   }
+  // };
+
+  
+  });
