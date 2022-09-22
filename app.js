aad3df58f3d64d394f2131f7cf234579949bfe89
aleksei <leshadjet@mail.ru>
2022-09-22 13:34:30 +0700

    final

--- app.js
+++ app.js
@@ -1,19 +1,57 @@
  $(document).ready(function(){
 
-  $(".search_button button").click(function(){
-      if ( $(".search_button input").hasClass("active") ) {
-        $('.search_button input').removeClass('active');
+    $(".search_button button").click(function(){
+        if ( $(".search_button input").hasClass("active") ) {
+          $('.search_button input').removeClass('active');
+      } else{
+          $('.search_button input').addClass('active');
+      }
+    });
+    $(".search_button button").click(function(){
+      if ( $(".btn-retract").hasClass("active") ) {
+        $('.btn-retract').removeClass('active');
     } else{
-        $('.search_button input').addClass('active');
+        $('.btn-retract').addClass('active');
     }
   });
-  $(".search_button button").click(function(){
-    if ( $(".btn").hasClass("active") ) {
-      $('.btn').removeClass('active');
+
+
+let  btnBurger = document.querySelector('.burger')
+
+function addburgerbtn(){
+  if ( btnBurger.clientWidth <= 448 ) {
+    $('.burger-main .btn').addClass('btn-retract');
+} else{
+  $('.burger-main .btn').removeClass('btn-retract');
+}
+};
+window.onload = function()
+{
+
+  $(".burger-header").click(function(){
+    if ( $(".burger").hasClass("active-burger") ) {
+      $('.burger').removeClass('active-burger');
   } else{
-      $('.btn').addClass('active');
+      $('.burger').addClass('active-burger');
   }
 });
-  
-  });
 
+
+addburgerbtn();
+}
+
+$(window).on("scroll resize", function() {
+  addburgerbtn();
+  });
+  
+  $('.scroll-to-block').click(function(e){
+    e.preventDefault();
+    let target = $(this).data('block');
+    $('html, body').animate({
+      scrollTop:$(target).offset().top
+    },700)
+  })
+
+
+
+});
